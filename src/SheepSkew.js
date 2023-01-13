6587563bb19b712dfca8bbfc3693e1f402088b3c
judepark <psw0616a@naver.com>
2023-01-13 23:04:49 +0900

    Solved

---
+++ src/SheepSkew.js
@@ -0,0 +1,6 @@
+function solution(n, k) {
+    var answer = 0;
+    answer = 12000 * n + 2000 * (k - Math.floor(n/10));
+    console.log(answer);
+    return answer;
+} // Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120830?language=javascript
