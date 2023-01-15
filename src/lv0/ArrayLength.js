7ac1c1f43e448350aa7d061b135296704a406056
judepark <psw0616a@naver.com>
2023-01-15 19:09:41 +0900

    Solved

---
+++ src/lv0/ArrayLength.js
@@ -0,0 +1,6 @@
+function solution(strlist) {
+    var answer = [];
+    for (let i = 0; i < strlist.length; i++)
+        answer[i] = strlist[i].length;
+    return answer;
+} // Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120854
