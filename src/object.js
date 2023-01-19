1838da560858f9662e9469a5a3a3d48a8af6cb7f
judepark <psw0616a@naver.com>
2023-01-19 14:48:35 +0900

    Study object of JavaScript

---
+++ src/object.js
@@ -0,0 +1,20 @@
+let person1 = {
+    name : 'lee'
+}
+let person2 = person1
+
+console.log(typeof person2)
+
+let variable1 = 80;
+let variable2 = variable1;
+console.log(variable1 ===  '80')
+const x = {
+    age : 1,
+    name : "Park"
+}
+console.log(x.name)
+x.name = "Lee"
+console.log(x.name)
+let name = {
+    age : 1,
+}
